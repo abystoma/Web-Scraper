164f318bbf2755ca9a8d1ef1021ff4a2d0fac11a
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-06-01 16:53:18 +0600

    Beautiful Soup Ingredients

--- scraper.py
+++ scraper.py
@@ -1,20 +1,26 @@
 import requests
+from bs4 import BeautifulSoup
 
-def retrieve_json(link: str) -> str:
-    response = link.json()
-    try:
-        quote = response["content"]
-        #dict.get(response, "content")
-    except KeyError:
-        return "Invalid quote resource!"
-    return quote
+def retrieve_json(soup: str) -> str:
+    json = {}
+    title = soup.find('title')
+    description = soup.find('meta',attrs={'name':"description"})['content'] #tag, attribute
+    json = {"title":title.text, "description":description}
+    return json
 
 def main() -> None:
     website = input("Input the URL:")
     r = requests.get(website)
     if r.status_code == 200:
-        print(retrieve_json(r))
+        soup = BeautifulSoup(r.content, 'html.parser')
+        check_movie = soup.find('meta', {'property': 'og:type'})
+        if check_movie['content']=='image':
+            print(retrieve_json(soup))
+        else:
+            print("Invalid movie page!")
     else:
-        print("Invalid quote resource!")
+        print("Invalid movie page!")
 
 main()
+
+
